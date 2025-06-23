82060e699b9bddb2a2aa64f4ebd613b6e32287ea
GuyShul <guysh6020@gmail.com>
2025-06-24 01:46:23 +0300

    feat: make set and range constraints generic

--- Constraintor.Core/Constraints/RangeConstraint.cs
+++ Constraintor.Core/Constraints/RangeConstraint.cs
@@ -1,28 +1,40 @@
+using Constraintor.Core.Utils;
+
 namespace Constraintor.Core.Constraints;
 
-public class RangeConstraint : Constraint
+public sealed class RangeConstraint<T>(
+    IEnumerable<string> targets,
+    IReadOnlyDictionary<string, List<ActivationCondition>> when,
+    T? min,
+    T? max,
+    bool lowerInclusive = false,
+    bool upperInclusive = false)
+    : Constraint(targets, when)
+    where T : IComparable<T>
 {
-    public int? Min { get; set; }
-    public int? Max { get; set; }
-
-    public override bool IsSatisfied(Dictionary<string, object> fieldsAssignment)
+    public override bool IsSatisfied(IReadOnlyDictionary<string, object?> fieldsAssignment)
     {
         if (!ShouldApply(fieldsAssignment))
             return true;
 
         foreach (var target in Targets)
         {
-            if (!fieldsAssignment.TryGetValue(target, out var val))
+            if (!fieldsAssignment.TryGetValue(target, out var rawValue) || rawValue is not T value)
                 continue;
 
-            if (val is not int i)
-                return false;
+            if (min is not null)
+            {
+                var cmp = value.CompareTo(min);
+                if (lowerInclusive ? cmp < 0 : cmp <= 0)
+                    return false;
+            }
 
-            if (Min.HasValue && i < Min.Value)
-                return false;
-
-            if (Max.HasValue && i > Max.Value)
-                return false;
+            if (max is null) continue;
+            {
+                var cmp = value.CompareTo(max);
+                if (upperInclusive ? cmp > 0 : cmp >= 0)
+                    return false;
+            }
         }
 
         return true;
--- Constraintor.Core/Constraints/SetConstraint.cs
+++ Constraintor.Core/Constraints/SetConstraint.cs
@@ -1,27 +1,39 @@
-using System.Text.Json.Serialization;
+using Constraintor.Core.Utils;
 
 namespace Constraintor.Core.Constraints;
 
-public class SetConstraint : Constraint
+/// <summary>
+/// Represents a constraint that limits field values to a specified set of allowed values.
+/// Generic type supportive, as long as the type inherit from <see cref="IEquatable{T}"/>.
+/// Thus,the values are expected to be convertible to their actual type.
+/// Moreover, the process logic relying on preliminary stage of parsing in the appropriate DTO layer.
+/// </summary>
+public class SetConstraint<T>(
+    IEnumerable<string> targets,
+    IReadOnlyDictionary<string, List<ActivationCondition>> when,
+    IReadOnlyList<T> allowedValues)
+    : Constraint(targets, when) where T : IEquatable<T>
 {
-    private IReadOnlyList<object> AllowedValues { get; } = [];
+    /// <summary>
+    /// A collection of optional valid values for the field within the constraint.
+    /// </summary>
+    public IReadOnlySet<T> AllowedSet => allowedValues.ToHashSet();
 
     /// <summary>
-    /// Default constructor required for deserialization.
+    ///  Determines whether the constraint is satisfied based on the current field assignments.
+    /// Constraint considers satisfied if there is a match between the assignment and in <c>_allowedSet</c> collection.
     /// </summary>
-    [JsonConstructor]
-    public SetConstraint()
-    {
-    }
-
-    public override bool IsSatisfied(Dictionary<string, object> fieldsAssignment)
+    /// <param name="fieldsAssignment">The current field-value mapping.</param>
+    /// <returns><c>true</c> if the constraint is satisfied, otherwise <c>false</c>.</returns>
+    public override bool IsSatisfied(IReadOnlyDictionary<string, object?> fieldsAssignment)
     {
         if (!ShouldApply(fieldsAssignment))
             return true;
 
         foreach (var target in Targets)
         {
-            if (!fieldsAssignment.TryGetValue(target, out var val) || !AllowedValues.Contains(val))
+            if (!fieldsAssignment.TryGetValue(target, out var val) || val is not T typed ||
+                !AllowedSet.Contains(typed))
                 return false;
         }
 
